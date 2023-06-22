726dbc1f53ada590de8f97847a1016d3a4941c5c
saisrinidhi8971 <saisrinidhi0009@gmail.com>
2023-06-22 23:08:32 +0530

    again some basic ui corrections

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import{Todo}from '../../TODO';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+  constructor(){
+    this.todos=[
+      {
+        sno:1,
+        title:"this is the title",
+        desc:"Description",
+        active:true
+      },
+      {
+        sno:2,
+        title:"this is the title",
+        desc:"Description",
+        active:true
+      },
+      {
+        sno:3,
+        title:"this is the title",
+        desc:"Description",
+        active:true
+      }
+    ]
+  }
+  ngOnInit(): void {
+
+  }
 
 }
